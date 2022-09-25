694772118765993301b723f61a32f019e43ad920
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-09-25 19:25:09 +0300

    55

---
+++ Exercise Data Types and Variables/T05_Triples of Latin Letters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T05_Triples_of_Latin_Letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
