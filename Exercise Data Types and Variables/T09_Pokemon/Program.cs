9b3073b541f5d6bdd86e405ab7e5eb9ec3184cf5
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-09-25 22:51:41 +0300

    Pokemon

---
+++ Exercise Data Types and Variables/T09_Pokemon/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace T09_Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nPokePower = int.Parse(Console.ReadLine());
+            int mDistance = int.Parse(Console.ReadLine());
+            int yExhaustion = int.Parse(Console.ReadLine());
+            int nPokeStart = nPokePower;
+            int targetCount = 0;
+            while (nPokePower >= mDistance)
+            {
+                if (nPokePower == nPokeStart/2 && nPokeStart%2 == 0 && yExhaustion != 0)
+                {
+                   nPokePower /= yExhaustion;
+                }
+                if(nPokePower >= mDistance)
+                {
+                    nPokePower -= mDistance;
+                    targetCount++;
+                }
+                
+                
+                
+            }
+            Console.WriteLine(nPokePower);
+            Console.WriteLine(targetCount);
+        }
+    }
+}
