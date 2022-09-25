4b3afd493270b74e9bda8e21da9b51e63aaded4f
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-09-25 18:30:14 +0300

    55

---
+++ Exercise Data Types and Variables/T06_Water Overflow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace T06_Water_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int n = int.Parse(Console.ReadLine());
+            int resCapacity = 255;
+            int resCurFill = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int flow = int.Parse(Console.ReadLine());
+                if (flow <= resCapacity)
+                {
+                    resCapacity -= flow;
+                    resCurFill += flow;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                
+            }
+            Console.WriteLine(resCurFill);
+        }
+    }
+}
---
+++ Exercise Data Types and Variables/T07_Triples of Latin Letters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace T07_Triples_of_Latin_Letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.Readline());
+            for (int i = 0; i < n; i++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        char firstChar = (char)('a' + i);
+                        char secondChar = (char)('b' + i);
+                        char thirdChar = (char)('c' + i);
+                       Console.WriteLine($"{firstChar}{secondChar}{thirdChar}");
+                    }
+                }
+            }
+        }
+    }
+}
