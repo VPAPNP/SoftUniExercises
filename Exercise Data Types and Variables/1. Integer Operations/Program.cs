eea0b0e922dfc7a2043bd484ae1a61a30fad395b
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-09-25 16:37:24 +0300

    Add exercises form data type
    add folder with more exercises

---
+++ Exercise Data Types and Variables/1. Integer Operations/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1._Integer_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+
+namespace T01_Integer_Operations
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int firstNum = int.Parse(Console.ReadLine());
+                int secondNum = int.Parse(Console.ReadLine());
+                int thirdNum = int.Parse(Console.ReadLine());
+                int fourthNum = int.Parse(Console.ReadLine());
+
+                int sumFirstSecond = firstNum + secondNum;
+                int divideThird = sumFirstSecond / thirdNum;
+                int multiply = divideThird * fourthNum;
+                Console.WriteLine(multiply);
+            }
+        }
+    }
+
+}
+    }
+}
---
+++ Exercise Data Types and Variables/T03_Elevator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace T03__Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numPeople = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int courses = (int)Math.Ceiling((double)numPeople / capacity);
+            Console.WriteLine(courses);
+        }
+    }
+}
