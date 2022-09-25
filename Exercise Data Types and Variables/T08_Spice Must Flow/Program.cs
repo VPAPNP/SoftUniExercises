1fcd82a32ee7589d35962ae997122722776623df
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-09-25 21:43:51 +0300

    new exercises
    add new 2

---
+++ Exercise Data Types and Variables/T08_Spice Must Flow/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace T08_Spice_Must_Flow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yield = int.Parse(Console.ReadLine());
+            int yieldSum = 0;
+            int days = 0;
+            while (yield >= 100)
+            {
+                days++;
+                yieldSum += yield;
+                if (yieldSum >= 26)
+                {
+                    yieldSum -= 26;
+                }
+                
+                
+                yield -= 10;
+            }
+            if (yieldSum >= 26)
+            {
+                yieldSum -= 26;
+            }
+            
+            Console.WriteLine(days);
+            Console.WriteLine(yieldSum);
+        }
+    }
+}
