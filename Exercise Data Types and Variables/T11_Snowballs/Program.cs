ba579238d9df20c197366e19cf9fbfb1a015b676
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-09-29 19:58:16 +0300

    snowballs

---
+++ Exercise Data Types and Variables/T11_Snowballs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+ namespace T11_Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+              int nnumBalls = int.Parse(Console.ReadLine());
+              BigInteger snowBallValueMax = BigInteger.MinusOne;
+            int snowBallSnowMax = int.MinValue;
+            int snowBallTimeMax = int.MinValue;
+            int snowBallQualityMax = int.MinValue;
+
+            for (int i = 0; i < nnumBalls; i++)
+            {
+                int snowBallSnow = int.Parse(Console.ReadLine());
+                int snowBallTime = int.Parse(Console.ReadLine());
+                int snowBallQuality = int.Parse(Console.ReadLine());
+                BigInteger snowBallValue = BigInteger.Pow (snowBallSnow / snowBallTime,snowBallQuality);
+                if (snowBallValue > snowBallValueMax)
+                {
+                    snowBallValueMax = snowBallValue;
+                    snowBallQualityMax = snowBallQuality;
+                    snowBallTimeMax = snowBallTime;
+                    snowBallSnowMax = snowBallSnow;
+                }
+
+                
+            }
+            Console.WriteLine($"{snowBallSnowMax} : {snowBallTimeMax} = {snowBallValueMax} ({snowBallQualityMax})");
+
+
+        }
+    }
+}
