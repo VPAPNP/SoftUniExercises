fe935a210815ad1aeb59f686effe832d29cb3e05
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-09-30 19:06:20 +0300

    new projects
    arrays

---
+++ Arrays/T1_Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace T1_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] day = new string[7];
+            day[0] = "Monday";
+            day[1] = "Tuesday";
+            day[2] = "Wednesday";
+            day[3] = "Thursday";
+            day[4] = "Friday";
+            day[5] = "Saturday";
+            day[6] = "Sunday";
+            int dayOfWeek = int.Parse(Console.ReadLine());
+            if (dayOfWeek > 7 || dayOfWeek < 1)
+            {
+                Console.WriteLine("Invalid day!");
+                return;
+            }
+
+            Console.WriteLine(day[dayOfWeek -1 ]);
+
+        }
+    }
+}
---
+++ Arrays/T2_Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T2_Print_Numbers_in_Reverse_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
