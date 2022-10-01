405c411376a5078fecb0e98a41251fe4bdcd1d2d
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-10-01 23:25:59 +0300

    new Project
    New Project

--- Arrays/ArraysExercise/T01_Train/Program.cs
+++ Arrays/ArraysExercise/T01_Train/Program.cs
@@ -6,7 +6,18 @@ namespace T01_Train
     {
         static void Main(string[] args)
         {
-             
+            int wagonsNum = int.Parse(Console.ReadLine());
+
+            int[] peopleNum = new int[wagonsNum];
+            int sum = 0;
+            for (int i = 0; i < wagonsNum; i++)
+            {
+                int people = int.Parse(Console.ReadLine());
+                peopleNum[i] = people;
+                sum += people;
+            }
+            Console.WriteLine(String.Join(" ", peopleNum));
+            Console.WriteLine(sum);
         }
     }
 }
---
+++ Arrays/ArraysExercise/T04_Array Rotation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace T04_Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();  
+           int rotationCount = int.Parse(Console.ReadLine());
+            int timesToRotate = rotationCount % arr.Length;
+            for (int r = 1; r <= timesToRotate; r++)
+            {
+                int firstElement = arr[0];
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    arr[i - 1] = arr[i];
+                }
+                arr[arr.Length - 1] = firstElement;
+            }
+            Console.WriteLine(String.Join(" ",arr));
+
+
+        }
+    }
+}
