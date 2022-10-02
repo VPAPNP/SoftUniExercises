13a65d461127b42211d17acc0020fa03118858b5
Vasil Prodanov <vasilprodanov@gmail.com>
2022-10-02 16:58:04 +0300

    neww

--- Arrays/ArraysExercise/T02_Common Elements/Program.cs
+++ Arrays/ArraysExercise/T02_Common Elements/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace T02_Common_Elements
 {
@@ -10,11 +11,45 @@ namespace T02_Common_Elements
                 .Split(" ");
             string[] second = Console.ReadLine()
                 .Split(" ");
+            string[] common = new string[first.Length + second.Length];
+            for (int i = 0; i < first.Length; i++)
+            {
+                string currentFirst = first[i];
+                for (int k = 0; k < second.Length; k++)
+                {
+                    string currentSecond = second[i];
+                    if (currentFirst == currentSecond)
+                    {
+                       common[i] = currentFirst;
+                    }
+                }
+                using System;
+                using System.Linq;
+
+
+public class Program
+        {
+            public static void Main()
+            {
+
+                int[] array1 = { 1, 4, 2, 8, 7 };
+                int[] array2 = { 7, 5, 9, 1, 0, 2, 6 };
+                // Call Intersect extension method.  
+                var intersect = array1.Intersect(array2);
+
+                foreach (int value in intersect)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+        }
+
+    }
 
 
 
-            Console.WriteLine(String.Join ( " ",first));
-            Console.WriteLine(String.Join(" ",second));
+            Console.WriteLine(String.Join ( " ",common));
+            
 
 
 
