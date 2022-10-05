e1333d40a1942d5c2ec71eee3f9d347046d32b55
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-10-05 08:39:24 +0300

    EqualSums
    EqualSums

---
+++ Arrays/ArraysExercise/T06_Equal Sums/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace T06_Equal_Sums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int sumLeft = 0;
+            int sumRight = 0;
+            foreach (int i in array)
+            {
+                
+
+                for (int j = 0; j < array.Length; j++)
+                {
+                     sumRight =+i;
+                    Console.WriteLine(sumRight);
+                    Console.WriteLine("Right---------------------");
+                }
+                
+
+                for (int c = array.Length; c > i; c--)
+                {
+                    sumLeft = +i;
+                    Console.WriteLine(sumLeft);
+                    Console.WriteLine("Left+++++++++++++++++++++");
+                }
+
+                if (sumLeft == sumRight)
+                {
+                    Console.WriteLine();
+                }
+               
+
+            }
+            
+            
+                
+
+        }
+    }
+}
