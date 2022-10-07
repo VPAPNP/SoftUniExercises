47e0f9ae01a13f649023feec0a045e717b0bb63e
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-10-07 21:06:24 +0300

    07.10.2022
    07.10.2022

---
+++ Arrays/ArraysExercise/T07_Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace T07_Max_Sequence_of_Equal_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray(); 
+            int equalNumSum = 0;
+            int maxSeq = 0;
+            int seqElement = 0;
+            
+            for (int i = 0; i < inputArray.Length -1; i++)
+            {
+
+                
+                if (inputArray[i] == inputArray[i+1] )
+                {
+                    equalNumSum++;
+                    if (equalNumSum > maxSeq)
+                    {
+                        maxSeq = equalNumSum;
+                        seqElement = inputArray[i];
+                    }
+                }
+                else
+                {
+                    
+                    equalNumSum = 0;
+                }
+                
+                
+            }
+            for (int i = 0; i <= maxSeq; i++)
+            {
+                Console.Write($"{seqElement} ");
+            }
+            
+        }
+    }
+}
---
+++ Arrays/ArraysExercise/T08_Magic Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace T08_Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine()
+                  .Split(" ")
+                  .Select(int.Parse)
+                     .ToArray();
+            int num = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < inputArray.Length -1; i++)
+            {
+                int sum = inputArray[i];
+                for (int j = 0; j < inputArray.Length; j++)
+                {
+                    
+                    if (sum + inputArray[j] == num)
+                    {
+                        Console.WriteLine($"{sum} {inputArray[i]} ");
+                    }
+                }
+                
+            }
+        }
+    }
+}
