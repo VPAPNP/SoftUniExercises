7fcc4300d2110fa74b528b62eb564d8adc4fb563
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-10-08 17:30:54 +0300

    MethodsExerciseNew
    Methods New Ex

---
+++ Methods/MethodsExercise/T02_Vowels Count/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T02_Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
