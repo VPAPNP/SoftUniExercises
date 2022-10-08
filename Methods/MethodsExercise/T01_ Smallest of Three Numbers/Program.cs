1463a51af47dfb8f23dfdb2689f42cbef334f437
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-10-08 12:41:01 +0300

    New Methods Exercise

---
+++ Methods/MethodsExercise/T01_ Smallest of Three Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T01__Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
