a6e35cc67ce592e56199bffc7aa8dc11f2aacf0f
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-10-09 22:46:38 +0300

    new Exercises

---
+++ Methods/MethodsExercise/T05_Add and Subtract/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace T05_Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int resultSum = SumTwoInt(first, second);
+            int finalRes = SubstarctInt(third, resultSum);
+            Console.WriteLine(finalRes);
+
+        }
+        static int SumTwoInt(int first , int second)
+        {
+            int result = first + second;
+            return result;
+        }
+        static int SubstarctInt(int third , int result)
+        {
+            int finalRes = result - third;
+            return finalRes;
+        }
+    }
+}
---
+++ Methods/MethodsExercise/T06_Middle Characters/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace T06_Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            text = PrintMidlleOfString(text);
+            Console.WriteLine(text);
+            
+        }
+        static string PrintMidlleOfString(string text)
+        {
+            char[] chars = text.ToCharArray();
+            string newRes = " ";
+            int charCount = chars.Length;
+            if (charCount % 2 == 0)
+            {
+                char[] resultOne = new char[2];
+                resultOne[0] = chars[(charCount / 2) - 1];
+                resultOne[1] = chars[charCount / 2];
+                newRes = new string(resultOne);
+            }
+            else
+            {
+                char[] resultOne = new char[1];
+                resultOne[0] = chars[charCount /2];
+                
+                newRes = new string(resultOne);
+            }
+            
+            return newRes;
+        }
+    }
+}
