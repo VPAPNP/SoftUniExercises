8f72dc269100b6a77ed10a1a061cc17fdd6222a3
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-10-12 23:23:21 +0300

    12.10.2022

--- Methods/MethodsExercise/T09_Palindrome Integers/Program.cs
+++ Methods/MethodsExercise/T09_Palindrome Integers/Program.cs
@@ -6,7 +6,38 @@ namespace T09_Palindrome_Integers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text = string.Empty;
+            PalindromeIntegers(text);
+        }
+        static void PalindromeIntegers(string text)
+        {
+            string reversed = string.Empty;
+
+            while (text != "END")
+            {
+                text = Console.ReadLine();
+                if (text == "END")
+                {
+                    break;
+                }
+                for (int i = text.Length -1; i >= 0; i--)
+                {
+                    reversed += text[i];
+                }
+                if (text == reversed)
+                {
+                    Console.WriteLine("true");
+                }
+                else if (text!= reversed)
+                {
+                    Console.WriteLine("false");
+                }
+                reversed = String.Empty;
+
+            }
+
+            
+           
         }
     }
 }
---
+++ Methods/MethodsExercise/T10_Top Number/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T10_Top_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
