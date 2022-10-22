34b62936b9591ade619c30f0aff3b5e07f44b195
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-10-22 22:36:27 +0300

    22.10.2022

---
+++ ExamPreparationFundamentals/ExamPreparationFundamentals/T04_SoftUni Reception/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace T04_SoftUni_Reception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstCapacity = int.Parse(Console.ReadLine());
+            int secondCapacity = int.Parse(Console.ReadLine());
+            int thirdCapacity = int.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            int sumCapacity = firstCapacity + secondCapacity + thirdCapacity;
+            int neededHours = 0;
+            while (students > 0)
+            {
+               students -= sumCapacity;
+                neededHours++;
+                if (neededHours% 4 == 0)
+                {
+                    neededHours++;
+                }
+
+            }
+
+            Console.WriteLine($"Time needed: {neededHours}h.");
+        }
+    }
+}
