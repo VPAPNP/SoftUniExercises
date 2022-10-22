fedd4228d1278390426cfebf952c4f7ab4569dc1
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-10-22 13:50:28 +0300

    22.03.2022

--- ExamPreparationFundamentals/ExamPreparationFundamentals/01. Guinea Pig/Program.cs
+++ ExamPreparationFundamentals/ExamPreparationFundamentals/01. Guinea Pig/Program.cs
@@ -6,7 +6,39 @@ namespace _01._Guinea_Pig
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            decimal food = decimal.Parse(Console.ReadLine());
+            decimal hay = decimal.Parse(Console.ReadLine());
+            decimal cover = decimal.Parse(Console.ReadLine());
+            decimal pigWeight =decimal.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 30; i++)
+            {
+                food -= 0.3m;
+                if ( i % 2 == 0)
+                {
+                    hay -= food * 0.05m;
+                }
+                if (i %3 == 0)
+                {
+                    cover -= pigWeight / 3;
+                }
+                if (food <= 0 || hay <= 0 || cover <= 0)
+                {
+                    
+                    break;
+                }
+
+            }
+            if (food <= 0 || hay <= 0 || cover <= 0)
+            {
+                Console.WriteLine("Merry must go to the pet store!");
+                
+            }
+            if (food > 0 && hay > 0 && cover > 0)
+            {
+                Console.WriteLine($"Everything is fine! Puppy is happy! Food: {food:f2}, Hay: {hay:f2}, Cover: {cover:f2}.");
+            }
+            
         }
     }
 }
