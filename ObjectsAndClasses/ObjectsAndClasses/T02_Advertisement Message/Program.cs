d02bc3d655de874449ec744f0842a84a7cfc7d5a
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-10-30 15:32:53 +0200

    30.10.2022

---
+++ ObjectsAndClasses/ObjectsAndClasses/T02_Advertisement Message/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T02_Advertisement_Message
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            List<string> phrase = new List<string> { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can’t live without this product." };
+            List<string> events = new List<string> { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+            List<string> author = new List<string> { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+            List<string> city = new List<string> { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+            Random random = new Random();
+            for (int i = 0; i < input; i++)
+            {
+                int indexPhrase = random.Next(0, phrase.Count);
+                int indexEvent = random.Next(0, events.Count);
+                int indexAuthor = random.Next(0, author.Count);
+                int indexCity = random.Next(0, city.Count);
+                Console.WriteLine($"{phrase[indexPhrase]} {events[indexEvent]} {author[indexAuthor]} – {city[indexCity]}.");
+
+            }
+
+        }
+    }
+
+   
+
+       
+
+           
+                
+        
+    
+    
+
+}
---
+++ ObjectsAndClasses/ObjectsAndClasses/T03_Articles/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace T03_Articles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine()
+                .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            int num = int.Parse(Console.ReadLine());
+            string content = input[1];
+            string author = input[2];
+            string title = input[0];    
+            
+            Article article = new Article(title, content, author);
+
+            for (int i = 0; i < num; i++)
+            {
+                string[] commandArg = Console.ReadLine().Split(": ",StringSplitOptions.RemoveEmptyEntries);
+                
+                
+                if (commandArg[0] == "Edit")
+                {
+                    article.Edit(commandArg[1]);
+
+                }
+                else if (commandArg[0] == "ChangeAuthor")
+                {
+                    article.ChangeAuthor(commandArg[1]);
+                }
+                else if (commandArg[0] == "Rename")
+                {
+                    article.Rename(commandArg[1]);
+                }
+            }
+            Console.WriteLine(article.ToString());
+                
+
+        }
+    }
+    public class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            this.Title = title;
+            this.Content = content; 
+            this.Author = author;
+           
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        
+
+        
+        
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+        public void Edit(string newContent)
+        {
+            this.Content = newContent;
+        }
+        public void Rename(string newTitle)
+        {
+            this.Title = newTitle;
+        }
+        public void ChangeAuthor(string newAuthor)
+        {
+            this.Author = newAuthor;
+        }
+    }
+}
