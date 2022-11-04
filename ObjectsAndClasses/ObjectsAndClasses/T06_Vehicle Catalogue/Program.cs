a6352c9d1d290c63a3d01d988a0a083643466fa4
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2022-11-04 23:47:51 +0200

    04.11.2022

---
+++ ObjectsAndClasses/ObjectsAndClasses/T06_Vehicle Catalogue/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace T06_Vehicle_Catalogue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            List<Vehicle> list = new List<Vehicle>();
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] vechicalInfo = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string type = vechicalInfo[0].ToLower();
+                string model = vechicalInfo[1];
+                string color = vechicalInfo[2].ToLower();
+                int horsePower = int.Parse(vechicalInfo[3]);
+                Vehicle vehicle = new Vehicle(type, model, color, horsePower);
+                list.Add(vehicle);
+            }
+
+                    List<int> numCars = new List<int>();
+                    List<int> numTrucks = new List<int> ();
+                    double horsePowerCars = 0;
+                    double horsePowerTrucks = 0;
+            while ((command = Console.ReadLine()) != "Close the Catalogue")
+            {
+
+                foreach (Vehicle vehicle in list)
+                {
+
+                    if (vehicle.Type == "car")
+                    {
+                        numCars.Add(1);
+                        horsePowerCars += vehicle.HorsePower;
+                    }
+                    else if (vehicle.Type == "truck")
+                    {
+                        numTrucks.Add(1);
+                        horsePowerTrucks += vehicle.HorsePower;
+                    }
+                    if (vehicle.Model == command)
+                    {
+                        Console.WriteLine(string.Join(Environment.NewLine, vehicle));
+                    }
+
+
+                }
+
+                
+
+            }
+            double averageHorsePowCars = horsePowerCars / numCars.Count;
+            double avarageHorsePowTrucks = horsePowerTrucks / numTrucks.Count;
+            if (numCars.Count > 0)
+            {
+                Console.WriteLine($"Cars have average horsepower of: {averageHorsePowCars:f2}.");
+            }
+            else
+            {
+                Console.WriteLine($"Cars have average horsepower of: {0:f2}.");
+            }
+            if (numTrucks.Count > 0)
+            {
+                Console.WriteLine($"Trucks have average horsepower of: {avarageHorsePowTrucks:f2}.");
+            }
+            else
+            {
+                Console.WriteLine($"Trucks have average horsepower of: {0:f2}.");
+            }
+        }
+    }
+    public class Vehicle
+    {
+        public Vehicle(string type, string model, string color, int horsePower)
+        {
+            Type = type;
+            Model = model;
+            Color = color;
+            HorsePower = horsePower;
+        }
+
+        public string Type { get; set; }
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+        public int HorsePower { get; set; }
+
+        public override string ToString()
+        {
+            string vehicaleStr = $"Type: {(this.Type == "car" ? "Car" : "Truck")}{Environment.NewLine}" +
+                                 $"Model: {this.Model}{Environment.NewLine}" +
+                                 $"Color: {this.Color}{Environment.NewLine}" + 
+                                 $"Horsepower: {this.HorsePower}";
+
+            return vehicaleStr;
+        }
+
+    }
+}
---
+++ ObjectsAndClasses/ObjectsAndClasses/T07_Order by Age/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace T07_Order_by_Age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            string input;
+            
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] inputArg = input.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+                string name = inputArg[0];
+                string id = inputArg[1];
+                int age = int.Parse(inputArg[2]);
+                
+                if (people.Any(x=> x.Id == id))
+                {
+                    for (int i = people.Count -1 ; i > 0; i--)
+                    {
+                        if (people[i].Id == id)
+                        {
+                            people[i].Age = age;
+                            people[i].Name = name;
+                            
+                        }
+                    }
+                    continue;
+                }
+                Person newPerson = new Person(name,id,age);
+                people.Add(newPerson);
+            }
+            List<Person> oreder = people.OrderBy(x => x.Age).ToList();
+            Console.WriteLine(string.Join(Environment.NewLine, oreder));
+            
+        }
+    }
+    public class Person
+
+    {
+        public Person(string name, string id, int age)
+        {
+            Name = name;
+            Id = id;
+            Age = age;
+        }
+
+        public string Name { get; set; } 
+        public string Id { get; set; }
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} with ID: {Id} is {Age} years old.";
+        }
+    }
+}
