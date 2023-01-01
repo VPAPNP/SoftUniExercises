37d81389f163cc341ca62a8eae15c749c1e27aba
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2023-01-01 18:59:29 +0200

    BasicStackOperations

--- C#Advanced/Stacks and Queues/T01_Basic Stack Operations/Program.cs
+++ C#Advanced/Stacks and Queues/T01_Basic Stack Operations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace T01_Basic_Stack_Operations
 {
@@ -6,7 +8,34 @@ namespace T01_Basic_Stack_Operations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] inputCommands = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            
+            int elementToLookFor = inputCommands[2];
+            int elementsToReacive = inputCommands[0];
+            int elementsToPop = inputCommands[1];
+            Stack<int> stack = new Stack<int>(input);
+            for (int i = 0; i < elementsToPop; i++)
+            {
+                if (stack.Count > 0)
+                {
+                    int element = stack.Pop();
+                }
+            }
+            if (stack.Contains(elementToLookFor))
+            {
+                Console.WriteLine("true");
+            }
+            else if (stack.Count == 0)
+            {
+                Console.WriteLine(stack.Count);
+            }
+            else 
+            {
+                  Console.WriteLine(stack.Min());
+            }
+
+
         }
     }
 }
