4cabe02770e69e1ea67b608bc2d781b859888e8e
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2023-01-02 15:39:24 +0200

    02.01.2023

---
+++ C#Advanced/Stacks and Queues/T03_Maximum and Minimum Element/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T03_Maximum_and_Minimum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+               int numQuery = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < numQuery; i++)
+            {
+                int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                int command = input[0];
+
+                if (command == 1)
+                {
+                    int num = input[1];
+                    stack.Push(num);
+                }
+                else if (command == 2)
+                {
+                    stack.Pop();
+
+                }
+                else if (command == 3 && stack.Count > 0)
+                {
+                    Console.WriteLine(stack.Max());
+                }
+                else if(command == 4 && stack.Count > 0) 
+                {
+                    Console.WriteLine(stack.Min());
+                }
+            }
+            if (stack.Count > 0)
+            {
+                Console.WriteLine(string.Join(", ", stack));
+            }
+            
+        }
+    }
+}
---
+++ C#Advanced/Stacks and Queues/T04_Fast Food/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T04_Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int orderTotalQuantity = int.Parse(Console.ReadLine());
+            int[] ordersCount = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>();
+            int biggest = int.MinValue;
+            
+            for (int i = 0; i < ordersCount.Length; i++)
+            {
+                queue.Enqueue(ordersCount[i]);
+                int cuurent = queue.Peek();
+                if (orderTotalQuantity >= cuurent)
+                {
+                    queue.Dequeue();
+                    orderTotalQuantity -= cuurent;
+                    if (cuurent > biggest)
+                    {
+                        biggest = cuurent;
+                    }
+                }
+            }
+           
+            
+                
+                
+            
+            Console.WriteLine(biggest);
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", queue)}");
+            }
+
+        }
+    }
+}
