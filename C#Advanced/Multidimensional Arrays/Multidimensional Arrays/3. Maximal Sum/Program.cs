1742dfdcdaf562fd6a9fe966813440ab79b3db68
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2023-01-06 16:28:41 +0200

    06.01.2023

--- C#Advanced/Multidimensional Arrays/Multidimensional Arrays/3. Maximal Sum/Program.cs
+++ C#Advanced/Multidimensional Arrays/Multidimensional Arrays/3. Maximal Sum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _3._Maximal_Sum
 {
@@ -6,7 +7,46 @@ namespace _3._Maximal_Sum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] rowsCols = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = rowsCols[0];
+            int cols = rowsCols[1];
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] ints = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = ints[col];
+                }
+            }
+            int maxSum = int.MinValue;
+            int sumRow = 0;
+            int sumCol = 0;
+            for (int row = 0; row < matrix.GetLength(0)-2; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1)-2; col++)
+                {
+                    int currenSum = matrix[row, col] +       matrix[row,col +1]      + matrix[row, col + 2]+
+                                    matrix[row + 1, col]+matrix[row+1, col + 1]+    matrix[row+1, col + 2]
+                                  + matrix[row +2, col ]   + matrix[row+2, col + 1]+    matrix[row+2, col + 2];
+                    if (currenSum > maxSum) 
+                    {
+                        maxSum = currenSum;
+                        sumRow = row;
+                        sumCol = col;
+                    }
+                }
+            }
+            Console.WriteLine($"Sum = {maxSum}");
+            for (int row = sumRow; row < sumRow +3; row++)
+            {
+                for (int col = sumCol; col < sumCol+3; col++)
+                {
+                    Console.Write($"{matrix[row,col]} ");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
---
+++ C#Advanced/Multidimensional Arrays/Multidimensional Arrays/4. Matrix Shuffling/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._Matrix_Shuffling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsCols = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = rowsCols[0];
+            int cols = rowsCols[1];
+            string[,] matrix = new string[rows, cols];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] strings = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = strings[col];
+                }
+            }
+            string command;
+            while ((command  = Console.ReadLine()) != "END")
+            {
+                string[] commandArg = command.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                if (commandArg[0] == "swap" && commandArg.Length == 5)
+                {
+                    int rowToSwap = int.Parse(commandArg[1]);
+                    int colToSwap = int.Parse(commandArg[2]);
+                    int rowSwapWith = int.Parse(commandArg[3]);
+                    int colSwapWith = int.Parse(commandArg[4]);
+                    if (rowToSwap <= matrix.GetLength(0) -1 && colToSwap <= matrix.GetLength(1) && rowSwapWith <= matrix.GetLength(0) - 1 && colSwapWith <= matrix.GetLength(1))
+                    {
+                        string first = matrix[rowToSwap,colToSwap];
+                        string second = matrix[rowSwapWith,colSwapWith];
+                        matrix[rowToSwap,colToSwap] = second;
+                        matrix[rowSwapWith,colSwapWith] = first;
+                        for (int row = 0; row < matrix.GetLength(0); row++)
+                        {
+                            for (int col = 0; col < matrix.GetLength(1); col++)
+                            {
+                                Console.Write($"{matrix[row,col]} ");
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
+        }
+    }
+}
