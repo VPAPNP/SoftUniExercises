1b26d1ad66bffac03b84bafb5197fd0031404ade
VPAPNP <113796638+VPAPNP@users.noreply.github.com>
2023-01-10 22:49:34 +0200

    10.01.2023

---
+++ C#Advanced/Sets and Dictionaries Advanced - Lab/Sets and Dictionaries Advanced - Lab/01. Count Same Values in Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<double,int> values= new Dictionary<double,int>();
+            double[] value = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            for (int i = 0; i < value.Length; i++)
+            {
+                double curr = value[i];
+                if (!values.ContainsKey(curr))
+                {
+                    values.Add(curr, 1);
+                }
+                else
+                {
+                    values[curr]++;
+                }
+            }
+            foreach (var item in values)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
